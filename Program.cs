3a0ce6bad415031867ef5863c75c81023d671a9f
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-16 12:08:55 -0700

    Injecting  Repository interface as a service

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 
 using MovieStore.Api.Endpoints;
+using MovieStore.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IMoviesRepository, InMemMoviesRepository>();
+
 var app = builder.Build();
 
 app.MapMoviesEndpoints();
