92e83637a68e5e4b6dc3c87e2ecf94d766ef41ba
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-17 10:37:51 -0700

    Generated Data-Migrations

--- obj/Debug/net8.0/MovieStore.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/MovieStore.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf48c138e67917fd3aa0e17b8145f1a247601fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b95674a31b479ed530663bcc523682d6aee0a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
