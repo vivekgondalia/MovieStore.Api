dec82f95edee929b6fa57f025627c9ed9a59a0b4
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-20 11:36:38 -0700

    API Endpoint only authorized form customer ROLE and for specific user. Need to create JWT token with specific CustomerName and assign them the role of CUSTOMER

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 
+using System.Security.Claims;
 using MovieStore.Api.Data;
 using MovieStore.Api.Endpoints;
 
@@ -12,8 +13,33 @@ var app = builder.Build();
 
 await app.Services.InitializeDbAsync();
 
-app.MapMoviesEndpoints().RequireAuthorization(policy => {
+app.MapMoviesEndpoints().RequireAuthorization(policy =>
+{
     policy.RequireRole("admin");
+});
+
+//Customers and their movies
+Dictionary<string, List<string>> moviesMap = new()
+{
+    {"Vivek Gondalia", new List<string>(){"The Dark Knight"}},
+    {"Customer 2", new List<string>(){"The Dark Knight Rises", "12 Angry Men"}},
+};
+
+// customer - get individual user movie list 
+app.MapGet("/mymovies", (ClaimsPrincipal user) =>
+{
+    ArgumentNullException.ThrowIfNull(user.Identity?.Name);
+    var username = user.Identity.Name;
+
+    if (!moviesMap.ContainsKey(username))
+    {
+        return Results.Empty;
+    }
+
+    return Results.Ok(moviesMap[username]);
+}).RequireAuthorization(policy =>
+{
+    policy.RequireRole("customer");
 });
 
 app.Run();
